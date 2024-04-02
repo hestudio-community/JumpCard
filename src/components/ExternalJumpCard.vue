186d573dbab7a251a0edd799375a64da19749b5d
undefined <zihengdie@hestudio.net>
2024-04-03 02:45:03 +0800

    优化代码

--- src/components/ExternalJumpCard.vue
+++ src/components/ExternalJumpCard.vue
@@ -1,79 +1,75 @@
 <template>
-    <el-card v-bind="card">
-        <div>
-            <el-text size="large" tag="b">{{ header }}</el-text>
-        </div>
-        <p>{{ text }}</p>
-        <div v-show="buttonshow">
-            <p>
-                <el-divider />
-            </p>
-            <el-button @click="jump" :icon="Link" link bg>{{ buttontext }}</el-button>
-        </div>
-    </el-card>
+  <el-card :header="header" :shadow="shadow">
+    <div>
+      <el-text size="large" tag="b">{{ header }}</el-text>
+    </div>
+    <p>{{ text }}</p>
+    <div v-show="showButton">
+      <p>
+        <el-divider />
+      </p>
+      <el-button @click="jump" :icon="Link" link bg>{{ buttontext }}</el-button>
+    </div>
+  </el-card>
 </template>
 
 <script setup>
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import { Link } from '@element-plus/icons-vue'
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import { Link } from "@element-plus/icons-vue";
 </script>
 
 <script>
 export default {
-    props: {
-        text: {
-            type: String,
-            default: ''
-        },
-        header: {
-            type: String,
-            default: ''
-        },
-        link: {
-            type: String,
-            default: ''
-        },
-        buttontext: {
-            type: String,
-            default: 'Jump to it!'
-        },
-        shadow: {
-            type: String,
-            default: "hover"
-        }
+  props: {
+    text: {
+      type: String,
+      default: "",
     },
-    data() {
-        return {
-            header: '',
-            text: '',
-            link: '',
-            buttontext: '',
-            card: {
-                header: '',
-                shadow: ''
-            },
-            buttonshow: false
-        }
-    }, 
-    methods: {
-        jump() {
-            window.open(this.link, "_blank")
-        }
+    header: {
+      type: String,
+      default: "",
     },
-    mounted() {
-        this.card.shadow = this.$props.shadow
-        if (this.$props.text != '' || this.$props.header) {
-            this.text = this.$props.text
-            if (this.$props.header != '') {
-                this.header = this.$props.header
-            }
-        }
-        if (this.$props.buttontext != '' && this.$props.link != '') {
-            this.buttonshow = true
-            this.buttontext = this.$props.buttontext
-            this.link = this.$props.link
-        }
+    link: {
+      type: String,
+      default: "",
+    },
+    buttontext: {
+      type: String,
+      default: "Jump to it!",
+    },
+    shadow: {
+      type: String,
+      default: "hover",
+    },
+  },
+  data() {
+    return {
+      // 使用props中定义的默认值作为data的初始值，避免数据冗余
+      showButton: false, // 更改变量名为showButton以匹配v-show指令的使用
+    };
+  },
+  methods: {
+    jump() {
+      // 确保打开新窗口的安全性，这里假设link已经在父组件中被验证过了
+      // 如果link来源不可信，需要在此处添加清理/验证逻辑
+      window.open(this.link, "_blank");
+    },
+  },
+  mounted() {
+    // 保持数据一致性，直接通过props更新data中的值
+    this.header = this.$props.header || "";
+    this.text = this.$props.text || "";
+    this.buttontext = this.$props.buttontext || "";
+    this.link = this.$props.link || "";
+
+    // 根据props值决定是否显示按钮
+    if (this.$props.buttontext !== "" && this.$props.link !== "") {
+      this.showButton = true;
     }
-}
+
+    // 直接将shadow赋值
+    this.shadow = this.$props.shadow;
+  },
+};
 </script>
--- src/components/InternalJumpCard.vue
+++ src/components/InternalJumpCard.vue
@@ -1,81 +1,55 @@
 <template>
-    <el-card v-bind="card">
-        <div>
-            <el-text size="large" tag="b">{{ header }}</el-text>
-        </div>
-        <p>{{ text }}</p>
-        <div v-show="buttonshow">
-            <p>
-                <el-divider />
-            </p>
-            <el-button @click="jump" :icon="Link" link bg>{{ buttontext }}</el-button>
-        </div>
-    </el-card>
+  <el-card :header="header" :shadow="shadow">
+    <div>
+      <el-text size="large" tag="b">{{ header }}</el-text>
+    </div>
+    <p>{{ text }}</p>
+    <div v-if="showButton">
+      <p>
+        <el-divider />
+      </p>
+      <el-button @click="jump" :icon="Link" link bg>{{ buttontext }}</el-button>
+    </div>
+  </el-card>
 </template>
 
-<script setup>
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import { Link } from '@element-plus/icons-vue'
-</script>
-
 <script>
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import { Link } from "@element-plus/icons-vue";
+
 export default {
-    props: {
-        text: {
-            type: String,
-            default: ''
-        },
-        header: {
-            type: String,
-            default: ''
-        },
-        link: {
-            type: String,
-            default: ''
-        },
-        buttontext: {
-            type: String,
-            default: 'Jump to it!'
-        },
-        shadow: {
-            type: String,
-            default: "hover"
-        }
+  props: {
+    text: {
+      type: String,
+      default: "",
     },
-    data() {
-        return {
-            header: '',
-            text: '',
-            link: '',
-            buttontext: '',
-            card: {
-                header: '',
-                shadow: ''
-            },
-            buttonshow: false
-        }
-    }, 
-    methods: {
-        jump() {
-            this.$router.push({
-                path: this.link
-            })
-        }
+    header: {
+      type: String,
+      default: "",
     },
-    mounted() {
-        this.card.shadow = this.$props.shadow
-        if (this.$props.text != '' || this.$props.header) {
-            this.text = this.$props.text
-            if (this.$props.header != '') {
-                this.header = this.$props.header
-            }
-        }
-        if (this.$props.buttontext != '' && this.$props.link != '') {
-            this.buttonshow = true
-            this.buttontext = this.$props.buttontext
-            this.link = this.$props.link
-        }
-    }
-}
+    link: {
+      type: String,
+      default: "",
+    },
+    buttontext: {
+      type: String,
+      default: "Jump to it!",
+    },
+    shadow: {
+      type: String,
+      default: "hover",
+    },
+  },
+  computed: {
+    showButton() {
+      return this.buttontext !== "" && this.link !== "";
+    },
+  },
+  methods: {
+    jump() {
+      this.$router.push({ path: this.link });
+    },
+  },
+};
 </script>
