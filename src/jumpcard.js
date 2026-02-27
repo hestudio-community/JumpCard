d9ff9f038ce08f43c5b5dbeab60fa30b57f4d955
undefined <hestudio@hestudio.net>
2026-02-27 23:50:40 +0800

    v26.1.1

--- src/jumpcard.js
+++ src/jumpcard.js
@@ -7,15 +7,24 @@ export default {
     app.component("InternalJumpCard", InternalJumpCard);
     app.component("ExternalJumpCard", ExternalJumpCard);
 
-    if (typeof options.externalButtonText == "function") {
-      app.provide("externalButtonText", options.externalButtonText);
-    }
-    if (typeof options.internalButtonText == "function") {
-      app.provide("internalButtonText", options.internalButtonText);
-    }
+    if (typeof options == "object") {
+      if (typeof options.externalButtonText == "function") {
+        app.provide("externalButtonText", options.externalButtonText);
+      }
+      if (typeof options.internalButtonText == "function") {
+        app.provide("internalButtonText", options.internalButtonText);
+      }
 
-    if (typeof options.onClickExternalLinkButton == "function") {
-      app.provide("onClickExternalLinkButton", options.onClickExternalLinkButton)
+      if (typeof options.onClickExternalLinkButton == "function") {
+        app.provide(
+          "onClickExternalLinkButton",
+          options.onClickExternalLinkButton,
+        );
+      }
+    } else {
+      app.provide("externalButtonText", undefined);
+      app.provide("internalButtonText", undefined);
+      app.provide("onClickExternalLinkButton", undefined);
     }
     app.component("jumpcard", JumpCard);
   },
